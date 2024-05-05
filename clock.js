a2cdc1a1e7592a29202e5918a0e13fcb880cbd98
ujjwalchauhan960 <161251952+ujjwalchauhan960@users.noreply.github.com>
2024-05-05 11:24:28 +0530

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,53 @@
+const second = document.querySelector("#second");
+const minute = document.querySelector("#minute");
+const hour = document.querySelector("#hour");
+
+const digitalSecond = document.querySelector("#digital-second");
+const digitalMinute = document.querySelector("#digital-minute");
+const digitalHour = document.querySelector("#digital-hour");
+
+const hand = document.querySelector(".hand");
+
+let min = 0;
+let sec = 0;
+let h = 0;
+
+const twoDigitNum = (num) => {
+  if (num < 10) {
+    return `0${num}`;
+  }
+  return num;
+};
+
+function clock() {
+  const date = new Date();
+  sec = date.getSeconds() * 6;
+  if (sec === 0) {
+    second.style.transition = "none";
+  } else {
+    second.style.transition = ".4s all ease-in";
+  }
+
+  if (min === 0) {
+    min = date.getMinutes() * 6;
+  }
+
+
+  min += 6 / 60;
+
+  if (h === 0) {
+    h = date.getHours() * 30;
+  }
+
+  h += 30 / (60 * 60);
+
+  second.style.transform = `rotate(${sec}deg)`;
+  minute.style.transform = `rotate(${min}deg)`;
+  hour.style.transform = `rotate(${h}deg)`;
+
+  digitalHour.innerHTML = twoDigitNum(date.getHours());
+  digitalMinute.innerHTML = twoDigitNum(date.getMinutes());
+  digitalSecond.innerHTML = twoDigitNum(date.getSeconds());
+}
+clock();
+setInterval(clock, 1000);
